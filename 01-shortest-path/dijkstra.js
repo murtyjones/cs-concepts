31c8ae2612c462efaf5d02438910ef4065d8bb8c
Marty Jones <murtin.jones@gmail.com>
2018-11-17 17:17:42 -0500

    incorrect but closer to correct implementation

--- 01-shortest-path/dijkstra.js
+++ 01-shortest-path/dijkstra.js
@@ -1,41 +1,36 @@
 const _ = require('lodash')
 const GraphNode = require('../utils/GraphNode')
 
-const length = (node, neighbor, nVisited) => {
+const length = (node, neighbor) => {
 
 }
 
 // 1. Mark all nodes unvisited.
 // Create a set of all the unvisited nodes called the unvisited set.
-const unvisited = {}
-for (let i = 1, l = 6; i <= l; i++) {
-  unvisited[`n${i}`] = new GraphNode({n:i, adj: []})
+const adjWithDistances = [
+  [ {n:1,w:3}, {n:2,w:2}, {n:4,w:4} ],
+  [ {n:0,w:3}, {n:2,w:8} ],
+  [ {n:0,w:2}, {n:1,w:8}, {n:3,w:1} ],
+  [ {n:2,w:1}, {n:4,w:3} ],
+  [ {n:0,w:4}, {n:3,w:3} ],
+]
+const unvisited = []
+for (let i=0, l=4; i <= 4; i++) {
+  unvisited.push(
+    new GraphNode({ n: i, adj: adjWithDistances[i] })
+  )
 }
-unvisited.n6.adj = [unvisited.n4]
-unvisited.n4.adj = [unvisited.n5, unvisited.n3]
-unvisited.n5.adj = [unvisited.n1, unvisited.n2, unvisited.n4]
-unvisited.n3.adj = [unvisited.n2, unvisited.n4]
 
 // 2. Set the initial node as current.
-const initialNode = unvisited.n6
-
 // Assign to every node a tentative distance value: set it to zero for
-// our initial node and to infinity for all other nodes.
+// our initial node and to infinity for all other unvisited.
 Object.values(unvisited).forEach(each => {
   each.distance = Number.POSITIVE_INFINITY
 })
-unvisited.n6.distance = 0
+const initialNode = unvisited[0]
+initialNode.distance = 0
 
-while (!_.isEmpty(unvisited)) {
-  const minDist = Math.min(...Object.values(unvisited).map(e => e.distance))
-  const node = _.find(unvisited, { distance: minDist })
-  delete unvisited[`n${node.n}`]
-  node.adj.forEach(neighbor => {
-    if (unvisited[`n${neighbor.n}`]) {
-      const alt = node.distance + length(node, neighbor)
-    }
-  })
-}
+
 
 // 3. For the current node, consider all of its unvisited neighbors
 // and calculate their tentative distances through the current
@@ -46,3 +41,21 @@ while (!_.isEmpty(unvisited)) {
 // B through A will be 6 + 2 = 8. If B was previously marked with a
 // distance greater than 8 then change it to 8. Otherwise,
 // keep the current value.
+const visited = []
+while (!!unvisited.length) {
+  const minDist = Math.min(...unvisited.map(e => e.distance))
+  const minDistIndex = _.findIndex(unvisited, { distance: minDist })
+  const n = unvisited[minDistIndex]
+  unvisited.splice(minDistIndex, 1)
+  visited.push(n)
+  n.adj.forEach(neighbor => {
+    const alt = n.distance + neighbor.w
+    const neighborNode = unvisited[neighbor.n]
+    console.log(neighborNode)
+    if (neighborNode && alt < neighborNode.distance) {
+      neighborNode.distance = alt
+    }
+  })
+}
+
+console.log(visited)
