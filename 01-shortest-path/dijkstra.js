7b2a286ba09cd2f138439bcab8937786712c1415
Marty Jones <murtin.jones@gmail.com>
2018-11-17 21:15:22 -0500

    dijkstra notes

--- 01-shortest-path/dijkstra.js
+++ 01-shortest-path/dijkstra.js
@@ -2,20 +2,19 @@ const _ = require('lodash')
 const GraphNode = require('../utils/GraphNode')
 
 const getLength = (u, v) => {
-  const i = _.findIndex(u.adj, { n: v.n })
-  const w = u.adj[i] || {}
+  const w = u.adj.filter(e => e.n === v.n)[0]
   return w.w
 }
 
-// 1. Mark all nodes unvisited.
-// Create a set of all the unvisited nodes called the unvisited set.
 const adjWithDistances = [
   [ {n:1,w:7}, {n:2,w:3} ],
   [ {n:0,w:7}, {n:2,w:1}, {n:3,w:2}, {n:4,w:6} ],
   [ {n:0,w:3}, {n:1,w:1}, {n:3,w:2} ],
-  [ {n:2,w:2}, {n:1,w:2}, {n:4,w:4} ],
-  [ {n:3,w:4}, {n:1,w:6} ],
+  [ {n:1,w:2}, {n:2,w:2}, {n:4,w:4} ],
+  [ {n:1,w:6}, {n:3,w:4} ],
 ]
+// create a set of unvisited nodes and place
+// all nodes inside of it to start with.
 const unvisited = []
 for (let i=0, l=4; i <= 4; i++) {
   unvisited.push(
@@ -23,33 +22,40 @@ for (let i=0, l=4; i <= 4; i++) {
   )
 }
 
-// 2. Set the initial node as current.
-// Assign to every node a tentative distance value: set it to zero for
-// our initial node and to infinity for all other nodes.
+// make every distance infinitely large as we
+// don't even know if it's possible to get
+// to every node from the starting point
+// until we start mapping.
 Object.values(unvisited).forEach(each => {
   each.distance = Number.POSITIVE_INFINITY
 })
+// set our starting node to a distance of zero
 unvisited[0].distance = 0
 
-
-
-// 3. For the current node, consider all of its unvisited neighbors
-// and calculate their tentative distances through the current
-// node. Compare the newly calculated tentative distance to the
-// current assigned value and assign the smaller one. For example,
-// if the current node A is marked with a distance of 6, and the edge
-// connecting it with a neighbor B has length 2, then the distance to
-// B through A will be 6 + 2 = 8. If B was previously marked with a
-// distance greater than 8 then change it to 8. Otherwise,
-// keep the current value.
+// create a set of visited nodes to add to.
 const visited = []
+
+// while some nodes remain unvisited:
 while (!!unvisited.length) {
+  // find the lowest distance node:
   const minDist = Math.min(...unvisited.map(e => e.distance))
   const minDistIndex = _.findIndex(unvisited, { distance: minDist })
   const n = unvisited.splice(minDistIndex, 1)[0]
   visited.push(n)
+
+  // for that lowest distance node, iterate through
+  // its neighbor nodes
   n.adj.forEach(neighbor => {
     const neighborNode = _.find(unvisited, { n: neighbor.n })
+    // for a given neighbor node:
+    // 1. get the distance between current
+    // node and neighbor node ("length").
+    // 2. add "length" to the distance
+    // of the current node from our initial node
+    // 3. if the length btw curr and neigh. node,
+    // + the distance of curr node to root node
+    // is less than the current neighbor node distance,
+    // we know that we can lower the neigh. node distance
     if (neighborNode) {
       const length = getLength(n, neighborNode)
       const alt = n.distance + length
@@ -60,4 +66,4 @@ while (!!unvisited.length) {
   })
 }
 
-visited.forEach(each => console.log(each.distance))
+visited.forEach(each => console.log(each.n, each.distance))
---
+++ 01-shortest-path/dijkstra.md
@@ -0,0 +1,47 @@
+## Dijkstra Method
+
+### Goal
+To find the shortest path from one node to another.
+
+### Method
+Pick a node as a starting point. Visit every neighbor of that node, see how far
+away it is, and save that distance. Then, pick the lowest distance, non-visited
+node, and repeat the process, adding the distance between the neighbor node to
+the current node's distance from the initial node, and so on.
+
+### Algorithm
+1. Mark every node unvisited or create a set of unvisited nodes and put all
+   inside of it to start with.
+2. Pick a starting node.
+3. Set all other nodes to have a distance of infinity to start with. Set the
+   starting node to a distance of zero.
+4. Visit from our "starting" node, and for each of its immediate neighbors:
+   1. Calculate starting node distance + distance from starting node to
+      neighbor node.
+   2. Compare this calculation to the neighbor's current distance value, and
+      assign the minimum of the two to be the neighbor's distance value.
+5. Repeat step 4 + sub steps, with the "starting" node being the one with the
+   lowest distance value that has not yet been visited. Repeat until the
+   destination node has been visited.
+
+### Pseudocode
+```
+function Dijkstra(graph):
+  for each in graph:
+    each.distance = Infinity
+  graph[0].distance = 0
+
+  unvisited = graph
+  visited = []
+  while unvisited is not empty:
+    n = node in unvisited set with the lowest distance value
+
+    remove n from unvisited and place into visited
+
+    for each_neighbor of n:
+      tentative_dist = n.distance + (distance between n and each_neighbor)
+      if (tentative_dist < each_neighbor.distance)
+        each_neighbor.distance = tentative_dist
+
+  return visited
+```
